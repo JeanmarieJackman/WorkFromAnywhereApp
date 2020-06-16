591232fdfc98d7be6d58f5f649f19b67f2f2b6cf
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-06-16 11:09:07 -0400

    home_controller added

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def welcome
+  end
+
+  def sign_in
+  end
+
+  def sign_out
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/', to: "home#welcome"
+  get '/sign_in', to: "home#sign_in"
+  get '/sign_out', to: "home#sign_out"
   resources :job_applications
   resources :job_listings
   resources :hiring_managers
