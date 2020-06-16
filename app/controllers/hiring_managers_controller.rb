2fe9dc55e99ea4eda78951a231a88dd7f9daf7c0
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-06-15 20:14:31 -0400

    job seeker crud and hiring manager show page added

--- app/controllers/hiring_managers_controller.rb
+++ app/controllers/hiring_managers_controller.rb
@@ -1,2 +1,38 @@
 class HiringManagersController < ApplicationController
+
+  before_action :get_hiring_manager , only: [:show,:edit,:update,:destroy]
+
+  def show
+
+  end
+
+  def new 
+    @hiring_manager = HiringManager.new
+  end
+
+  def create 
+
+  end 
+
+  def edit 
+
+  end 
+
+  def update 
+
+  end
+
+  def destroy 
+
+  end
+
+  private 
+
+  def hiring_manager_params 
+
+  end
+
+  def get_hiring_manager 
+    @hiring_manager = HiringManager.find(params[:id])
+  end
 end
--- app/controllers/job_seekers_controller.rb
+++ app/controllers/job_seekers_controller.rb
@@ -1,2 +1,63 @@
 class JobSeekersController < ApplicationController
+
+  before_action :get_job_seeker,only: [:show,:edit,:update,:destroy]
+  
+  def index 
+    @job_seekers = JobSeeker.all
+  end
+
+  def show 
+
+  end
+
+  def new 
+    @job_seeker = JobSeeker.new 
+  end
+
+  def create 
+    @job_seeker = JobSeeker.new(job_seeker_params)
+    if @job_seeker.save
+      redirect_to job_seeker_path(@job_seeker)
+    else
+      flash[erros] = @job_seeker.errors.full_messages
+    end
+  end 
+
+  def edit 
+
+  end
+
+  def update 
+    if @job_seeker.update(job_seeker_params)
+      redirect_to job_seeker_path(@job_seeker)
+    else
+      flash[erros] = @job_seeker.errors.full_messages
+    end
+  end
+
+  def destroy
+    @job_seeker.destroy
+    redirect_to job_seekers_path
+  end
+
+  private 
+
+  def job_seeker_params 
+    params.require(:job_seeker).permit(
+      :name,
+      :email,
+      :password,
+      :address,
+      :profile_picture,
+      :phone,
+      :resume_link,
+      :website_link,
+      :title_tagline
+      )
+  end
+
+  def get_job_seeker 
+    @job_seeker = JobSeeker.find(params[:id])
+  end
+
 end
